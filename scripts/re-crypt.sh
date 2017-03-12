6f6af9eefc08c2dd83b1c8a62e2a1107cbadb76d
Nayeem Syed <developerinlondon@gmail.com>
2017-03-12 16:02:15 +0000

    gitcrypt

--- scripts/re-crypt.sh
+++ scripts/re-crypt.sh
@@ -1,15 +1,70 @@
 #!/usr/bin/env bash
+#
+# Script to remove GPG key from git-crypt
+#
+# It will re-initialize git-crypt for the repository and re-add all keys except
+# the one requested for removal.
+#
+# Note: You still need to change all your secrets to fully protect yourself.
+# Removing a user will prevent them from reading future changes but they will
+# still have a copy of the data up to the point of their removal.
+#
+# Use:
+#  ./remove-gpg-user.sh [FULL_GPG_FINGERPRINT [FULL_GPG_FINGERPRINT]]
+#
+# E.g.:
+#  ./remove-gpg-user.sh 3BC18383F838C0B815B961480F8CAF5467D ABCD8383F838C0B815B961480F8CAF5467D
+#
+# The script will create multiple commits to your repo. Feel free to squash them
+# all down to one.
+#
+# Based on https://github.com/AGWA/git-crypt/issues/47#issuecomment-212734882
+#
+#
+set -e
 
+TMPDIR=`mktemp -d`
+echo "Using TMPDIR $TMPDIR"
+CURRENT_DIR=`git rev-parse --show-toplevel`
+BASENAME=$(basename `pwd`)
+
+# Unlock the directory, we need to copy encrypted versions of the files
 git crypt unlock
-rm -rf .git-crypt
-rm -rf .git/git-crypt
-git commit -a -m "Remove git-crypt"
 
+# Work on copy.
+cp -rp `pwd` $TMPDIR
+
+
+pushd $TMPDIR/$BASENAME
+
+# Remove encrypted files and git-crypt
+git crypt status | grep -v "not encrypted" > encrypted-files
+awk '{print $2}' encrypted-files | xargs rm
+git commit -a -m "Remove encrypted files"
+rm -rf .git-crypt
+git commit -a -m "Remove git-crypt"
+rm -rf .git/git-crypt
+
+# Re-initialize git crypt
 git crypt init
 
 while read p; do
   git crypt add-gpg-user --trusted $p
 done <.gpgusers
 
+
+cd $CURRENT_DIR
+for i in `awk '{print $2}' ${TMPDIR}/${BASENAME}/encrypted-files`; do
+    rsync -R $i $TMPDIR/$BASENAME;
+done
+cd $TMPDIR/$BASENAME
+for i in `awk '{print $2}' encrypted-files`; do
+    git add $i
+done
+git commit -a -m "New encrypted files"
+popd
+
 git crypt lock
-git crypt unlock
+git pull $TMPDIR/$BASENAME
+
+rm -rf $TMPDIR
--- scripts/remove-gpg-user.sh
+++ scripts/remove-gpg-user.sh
@@ -21,7 +21,7 @@
 # Based on https://github.com/AGWA/git-crypt/issues/47#issuecomment-212734882
 #
 #
-set -ex
+set -e
 
 if [ -z "$1" ]
 then

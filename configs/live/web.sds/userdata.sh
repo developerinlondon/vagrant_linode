902893b64ccbc51b70ce7c58da67e91feab3a692
Nayeem Syed <nayeem.syed@hmcts.net>
2017-03-14 13:05:21 +0000

    fixed hostname config

--- configs/live/web.sds/userdata.sh
+++ configs/live/web.sds/userdata.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+HOSTNAME=$(cat /etc/hostname)
+sed -i -e "s/127.0.0.1\slocalhost$/127.0.0.1 localhost $HOSTNAME/g" /etc/hosts
 apt-get install -y salt-api salt-cloud salt-master  salt-minion salt-ssh salt-syndic
